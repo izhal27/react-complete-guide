7025e75fe480dc42ee953b6633b8433ef2de41eb
Risal Walangadi <risal.walangadi@gmail.com>
2020-06-08 16:12:27 +0800

    add burger and ingredients component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <Layout>
+      <div>
+        <BurgerBuilder />
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    },
+  };
+
+  render() {
+    return (
+      <>
+        <Burger ingredients={this.state.ingredients} />
+        <h1>Build Controls</h1>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
