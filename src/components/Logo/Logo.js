cd143479c19a5a126d321ac859afac761d5bfac5
Risal Walangadi <risal.walangadi@gmail.com>
2020-06-14 17:16:08 +0800

    add logo and navigation items

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './Logo.module.css';
+import burgerLogo from './../../assets/images/burger-logo.png';
+
+const logo = () => (
+  <div className={styles.Logo}>
+    <img src={burgerLogo} alt="Burger logo" />
+  </div>
+);
+
+export default logo;
