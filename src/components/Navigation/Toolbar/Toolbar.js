81f6800c7ccb2867ecf65c7b2a63ad1cca6e2c0b
Risal Walangadi <risal.walangadi@gmail.com>
2020-06-15 15:56:05 +0800

    fix navigation mobile

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = () => (
     <div className={styles.Logo}>
       <Logo />
     </div>
-    <nav>
+    <nav className={styles.DesktopOnly}>
       <NavigationItems />
     </nav>
   </header>
