661c2be98c5b7213ca13d9bceeb789ff3af71acf
Risal Walangadi <risal.walangadi@gmail.com>
2020-06-16 15:33:45 +0800

    fix toggle button menu

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from './../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems';
+import DrawToggle from '../SideDrawer/DrawToggle/DrawToggle';
 
-const toolbar = () => (
+const toolbar = (props) => (
   <header className={styles.Toolbar}>
-    <div>Menu</div>
+    <DrawToggle clicked={props.drawerToggleClicked} />
     <div className={styles.Logo}>
       <Logo />
     </div>
